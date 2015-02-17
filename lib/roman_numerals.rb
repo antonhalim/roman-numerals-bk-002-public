38691d69bb3e4cdc19e9121ee352246c80cd7812
axton21 <axton_n@yahoo.com>
2015-02-17 18:28:16 -0500

    made me think twice

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+class Integer
+  require 'pry'
+
+  def to_roman
+    result = ""
+    number = self
+    numerals = {
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
+    numerals.keys.each do |divisor|
+      quotient, modulus = number.divmod(divisor)
+      result << numerals[divisor] * quotient
+      number = modulus
+    end
+    result
+
+  end
+
+end
